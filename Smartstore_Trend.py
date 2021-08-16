778f7d265ca985582a2b06da002394e328056697
itmakesmesoft <dmsgur7112@gmail.com>
2021-08-16 19:15:32 +0900

    코드 단순화 및 엑셀 출력기능 추가

--- Smartstore_Trend.py
+++ Smartstore_Trend.py
@@ -1,9 +1,13 @@
+from bs4.element import ProcessingInstruction
+import pandas as pd
+import openpyxl
+import time
 import requests # 크롤링에 사용하는 패키지
-from bs4 import BeautifulSoup # html 변환에 사용함
 from datetime import datetime, timedelta # import time
 import json
-from requests import status_codes
-from requests.models import Response
+# from bs4 import BeautifulSoup # html 변환에 사용함
+# from requests import status_codes
+# from requests.models import Response
 
 url = 'https://datalab.naver.com/shoppingInsight/getCategoryClickTrend.naver'
 # 헤더정보
@@ -12,12 +16,12 @@ header = { # 유저의 정보를 입력(차단을 막기 위함)
     'referer': 'https://datalab.naver.com/shoppingInsight/sCategory.naver'
 }
 #-------------------------Setting------------------------
-res_index=0
-stdValue = {}
-firstCid=181  # 탐색을 시작할 cid 값
-secondCid=188  # 탐색을 종료할 cid 값 | cidStart와 cidEnd를 같은 값으로 입력 시 오류 발생
-cont='y'
-rangeDate = 3 # 탐색 기간(일)
+res_index=0 # 크롤링 된 결과값에서 필요한 데이터의 인덱스
+stdValue = {} # 기준으로 할 메인 cid의 value 값이 담길 딕셔너리
+firstCid=84  # 탐색을 시작할 cid 값
+secondCid=87  # 탐색을 종료할 cid 값 | cidStart와 cidEnd를 같은 값으로 입력 시 오류 발생
+cont='y' # y:범위 내 연속된 모든 카테고리 탐색 | n: 입력한 두 개의 카테고리만 탐색
+rangeDate = 30 # 탐색 기간(일)
 now = datetime.now() # 현재 시간
 startDate = (now - timedelta(days=rangeDate+1)).strftime('%Y-%m-%d') # 오늘 날짜에서 rangeDate+1만큼 뺌
 endDate = (now - timedelta(days=1)).strftime('%Y-%m-%d') # 오늘 날짜에서 1만큼 뺌
@@ -25,61 +29,41 @@ endDate = (now - timedelta(days=1)).strftime('%Y-%m-%d') # 오늘 날짜에서 1
 
 
 #--------------------------------------------------------
-def crawling(firstCid, secondCid, rangeDate, res_index): #res_index: 구하고자 하는 res 내 인덱스 값
-    wrapData={}
+
+def crawling(firstCid, secondCid, rangeDate, res_index): # 네이버 쇼핑인사이트 데이터 크롤링하는 함수 | res_index: 구하고자 하는 res 내 인덱스 값
     try: # cid가 없는 경우 index error 발생 ==> index error 발생 시 pass
         data={"cid": [50000000+firstCid, 50000000+secondCid], "timeUnit": "date", "startDate": startDate, "endDate": endDate} # form값을 입력
         Fdata = requests.post(url, headers=header, data=data) # url에 url 정보를 넣고, headers(유저의 정보)에 header변수를, data에 data(입력된 form값)을 전송 후 데이터 요청
         if Fdata.status_code == requests.codes.ok:
-            # print("try")
             res = json.loads(Fdata.text)["result"] #json.loads는 json 문자열을 파이썬 객체로 변환/ 반대로 json.dumps()는 파이썬 객체를 json 문자열로 변환
-            code = res[res_index]["code"]
-            title = res[res_index]["title"]
-            fullTitle = res[res_index]["fullTitle"]
-            catClass = len(fullTitle.split(' > '))
+            # fullTitle = res[res_index]["fullTitle"]
             if res[res_index]['data']: # ---------------데이터 존재유무 검증
-                for j in range(rangeDate): # ----------------------------크롤링할 기간
-                    # print("44: ",res_index)
-                    period = res[res_index]["data"][j]["period"]
-                    value = res[res_index]["data"][j]["value"]
-                    if res_index==0:
-                        stdValue[j] = value
-                    elif res_index==1:
-                        valueA = stdValue[j]
-                        valueB = res[0]["data"][j]["value"]
-                        valueD = res[1]["data"][j]["value"]
-                        value = round(valueA*valueD/valueB, 10) # cid 50000000번 값이 50000000+cid 값에 따라 달라질 수도 있어 stdValue 기준값을 이용해 비례식 계산 | 기준cid값(50000000) : X = 현재cid값(50000000) : 현재cid값(50000002)
-                        # print("valueA:", valueA, " valueC:", valueB, " valueD:", valueD, " value:", value)
-                    # print("pass")
-                    resData = {"code":code, "title":title, "fullTitle":fullTitle, "catClass":catClass, "period":period, "value":value}
-                    wrapData[j] = resData
-                # print(res_index, wrapData)
-                return wrapData
+                return [res[res_index]]
         else:
             print("Error code", Fdata.status_code)
             return
     except: # index error 발생 시 pass
         return
 #----------------------------------------------------------
+resData={}
 if cont=="y":
-    frst=crawling(firstCid, secondCid, rangeDate, 0)
-    if frst:
+    a=crawling(firstCid, secondCid, rangeDate, 0)
+    if a:
         for i in range(firstCid, secondCid):
             b=crawling(firstCid, i+1, rangeDate, 1)
             if b:
-                if frst!="done":
+                if a!="done":
                     a=crawling(firstCid, i+1, rangeDate, 0)
-                    for index in range(0, rangeDate):
-                        print(a[index])
-                    frst="done"
-                print("-"*110) # ---------------------------------카테고리별 구분선
+                    resData=a
+                    a="done"
                 b=crawling(firstCid, i+1, rangeDate, 1)
-                for index in range(0, rangeDate):
-                    print(b[index])
+                resData.append(b[0])
             elif not b and i==secondCid:
                 print("카테고리 b가 존재하지 않음")
-            if cont=="n":
-                break
+            print("진행상황",i-firstCid+1,"/",secondCid-firstCid)
+            if i!=0 and i% 25 == 0:
+                time.sleep(2.7)
+            time.sleep(0.3)
     else:
         print("카테고리 a가 존재하지 않음")
 elif cont=="n":
@@ -88,9 +72,28 @@ elif cont=="n":
     for i in range(0,2):
         for index in range(0, rangeDate):
             if i==0:
-                print(a[index])
+                resData=a
             else:
-                print(b[index])
+                resData.append(b[0])
         if i==0:   
-            print("-"*110) # ---------------------------------카테고리별 구분선
+            print("-"*110) # 카테고리별 구분선
+
+for i in range(0, len(resData)):
+    a=0
+    print("후처리중",i+1,"/",len(resData))
+    for j in range(0,len(resData[i]["data"])):
+        a += resData[i]["data"][j]["value"]
+    if resData[i]["fullTitle"] != resData[i]["title"] and resData[i]["fullTitle"]:
+        catClass=len(resData[i]["fullTitle"].split(' > '))
+        resData[i]["catClass"] = catClass
+    else:
+        resData[i]["catClass"] = 1
+    average=a/rangeDate
+    resData[i]["data"] = average
+
+
+resData=pd.DataFrame(data=resData)
+resData = resData.set_index('code')
+print(resData)
 print("[기간 :", startDate, "~", endDate+"]") # 기간 표시
+resData.to_excel('네이버 통계.xlsx')
